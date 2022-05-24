611fa883e6e5306535380dadeb16d1e8d446b0c7
kinda-cookie-monster <105805414+kinda-cookie-monster@users.noreply.github.com>
2022-05-24 11:22:18 +0300

    `Notes` class deprecated, automatic folder creation
    The `Notes` class code is removed from the `classes` module. The reason for deprecation is that the code was a functional mirror of the `Homework` class, the endpoints were missing and there was no use for that code.
    
    Now authentication folders (i.e. `codes/`, `tokens/`) are created automatically upon the import of the `classes` module.

--- classes/__init__.py
+++ classes/__init__.py
@@ -1,15 +1,20 @@
-#baselibs
+import os
+
 from .cabinet import *
 from .homework import *
 from .lesson import *
 from .news import *
-from .notes import *
 from .user import *
 
-#timetable
 from .timetable.replacements import *
 from .timetable.timetable import *
 
-#surverys
 from .surveys.survey import *
 from .surveys.vote import *
+
+# Creating authentication folders if there are none
+folders_to_create = ["codes", "tokens", "tokens/access", "tokens/refresh"]
+
+for folder in folders_to_create:
+    if os.path.exists(folder) != True:
+        os.mkdir(folder)
