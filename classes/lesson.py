f679e6b1b697b1fc351c996bf4364aca9d29635e
Dmitriy <16977314+superdima05@users.noreply.github.com>
2022-07-31 23:33:24 +0300

    Timetable final fix.
    Или нет....

--- classes/lesson.py
+++ classes/lesson.py
@@ -1,85 +1,149 @@
-class Lesson():
-	def __init__(self, school, dictionary_object = None):
-		if dictionary_object is None:
-			self.id = None
-			self.subject = None
-			self.day = None
-			self.position = None
-			self.unique = False
-			self.cabinet = None
-			self.class_number = None
-			self.class_letter = None
-			self.teacher = None
+import json
+from datetime import datetime
+
+from flask import Response
+
+import classes
+from .helpers import *
+from .authentication import get_profile 
+
+
+def timetable_today(request, school):
+	return timetable(request, school, datetime.now())
+
+
+def timetable_date(request, school, date):
+	date = datetime.strptime(date, '%d.%m.%Y')
+	return timetable(request, school, date)
+
+
+def timetable(request, school, date):
+	if (profile := get_profile(school, request)) is None:
+		return e403()
+
+	lessons_query = {
+		"day": date.weekday(),
+		"class_number": profile.classNumber,
+		"class_letter": profile.classLetter
+	}
+	replacements_query = {"date": date.strftime("%d.%m.%Y")}
+
+	found_lessons = classes.Lesson.search(school, lessons_query)
+	found_replacements = classes.Replacement.search(school, replacements_query)
+	sorted_lessons = {}
+	
+	for lesson in found_lessons:
+		if lesson.position not in sorted_lessons.keys():
+			sorted_lessons[lesson.position] = classes.Lesson(school)
+		
+		if sorted_lessons[lesson.position].unique:
+			pass
+		elif lesson.unique and not sorted_lessons[lesson.position].unique:
+			if lesson.id in profile.uniqLessons:
+				sorted_lessons[lesson.position] = lesson
 		else:
-			self.__dict__ = dictionary_object
-			if "_id" in self.__dict__.keys():
-				del self.__dict__["_id"]
+			sorted_lessons[lesson.position] = lesson
+		
+	timetable = []
+	replacements = {item.lesson_id: item for item in found_replacements}
 
-		self.school = school
+	for lesson in sorted_lessons.values():
+		if lesson == classes.Lesson(school):
+			pass
 
-	def to_dict(self):
-		dictionary = self.__dict__.copy()
-		del dictionary["school"]
-		return dictionary
-
-	def to_old_dict(self):
-		# This is used to provide backwards compatibility with the previous
-		# versions still in use.
-		return {
-			"id": self.id,
-			"title": self.subject,
-			"teacher": self.teacher,
-			"cabinet": self.cabinet,
-			"classNumber": self.class_number,
-			"classLetter": self.class_letter 
-		}
-
-	def save_to_db(self):
-		database = self.school.database.lessons
-		dictionary = self.to_dict()
-		if database.count_documents(dictionary) > 0:
-			return False
-
-		# Getting an entry with the largest identifier
-		dictionary["id"] = database.count_documents({})
-
-		# Saving the new entry into the database
-		return database.insert_one(dictionary)
-
-	def save_changes(self):
-		database = self.school.database.lessons
-		return database.replace_one({"id": self.id}, self.to_dict())
-
-	def delete(self):
-		return self.school.database.lessons.delete_one({"id": self.id})
-
-	def search(school, query):
-		found_objects = []
-		search_results = school.database.lessons.find(query)
-
-		for result in search_results:
-			found_objects.append(Lesson(school, result))
-
-		return found_objects
-
-	def with_id(school, id):
-		if result := Lesson.search(school, {"id": id}):
-			return result[0]
+		if not replacements or lesson.id not in replacements.keys():
+			is_replaced = False
+			original_lesson = classes.Lesson(school)
 		else:
-			return None
+			is_replaced = True
+			original_lesson = lesson
+			replacement = replacements[lesson.id]
 
-	def with_subject(school, subject_title):
-		return Lesson.search(school, {"subject": subject_title})
+			if replacement.subject:
+				lesson.subject = replacement.subject
+			if replacement.position:
+				lesson.position = replacement.position
+			if replacement.cabinet:
+				lesson.cabinet = replacement.cabinet
+			if replacement.teacher:
+				lesson.teacher = replacement.teacher
 
-	def with_day(school, day):
-		return Lesson.search(school, {"day": day})
+		teacherUser = classes.User.with_id(school, int(lesson.teacher))
+		if teacherUser != None:
+			lesson.teacher = teacherUser.surname + " " + teacherUser.name + " " + teacherUser.middleName
 
-	def with_cabinet(school, cabinet_number):
-		return Lesson.search(school, {"cabinet": cabinet_number})
+		cabinet = classes.Cabinet.with_number(school, int(lesson.cabinet))
+		if cabinet != None:
+			lesson.cabinet = cabinet.to_dict()
 
-	def with_class(school, number, letter):
-		query = {"class_number": number, "class_letter": letter}
-		return Lesson.search(school, query)
+		# This is a temporary measure to keep backwards compatibility
+		# constructed_lesson = lesson.to_old_dict()
+		# constructed_lesson["replacement"] = is_replaced
+		# constructed_lesson["originalLesson"] = original_lesson.to_old_dict()
+		timetable.append(lesson.to_dict())
+	
+	response = Response(json.dumps(timetable), status = 200)
+	response.headers['Content-Type'] = 'application/json'
+	return response
 
-	def findByTeacher(school, teacher_id):
-		return Lesson.search(school, {"teacher": teacher_id})
+
+def get_classes(request, school):
+	if (profile := get_profile(school, request)) is None:
+		return e403()
+	if 'role' not in profile.flags or profile.flags['role'] == 0:
+		return e403()
+
+	timetable = school.database.timetable
+
+	class_numbers = timetable.distinct("classNumber")
+	class_letters = timetable.distinct("classLetter")
+
+	all_classes = []
+
+	for classNumber in class_numbers:
+		for classLetter in class_letters:
+			all_classes.append({"classNumber": classNumber, "classLetter": classLetter})
+
+	response = Response(json.dumps(all_classes), status = 200)
+	response.headers['Content-Type'] = 'application/json'
+	return response
+
+
+def get_class_timetable(request, school):
+	if (profile := get_profile(school, request)) is None:
+		return e403()
+	if 'role' not in profile.flags or profile.flags['role'] == 0:
+		return e403()
+
+	class_number = request.args.get("classNumber")
+	class_letter = request.args.get("classLetter")
+
+	if class_number is None or class_letter is None:
+		return e400()
+
+	query = {
+		"class_number": int(class_number),
+		"class_letter": class_letter
+	}
+	lessons = list(school.database.lessons.find(query))
+	if len(lessons) == 0:
+		return e404()
+
+	final = {
+		0: [],
+		1: [],
+		2: [],
+		3: [],
+		4: [],
+		5: [],
+		6: []
+	}
+	
+	for lesson in lessons:
+		del lesson["_id"]
+		day = lesson["day"]
+		final[day].append(lesson)
+			
+	response = Response(json.dumps(final), status = 200)
+	response.headers['Content-Type'] = 'application/json'
+	return response
